0b5ac28c4470c3682d248c150ae84bcc1c0ffd9d
Liu von Engelbrechten <vonengelbrechten.l@northeastern.edu>
2025-11-24 10:23:23 -0500

    Replaced phone number with email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         </p>
         <p>
             vonengelbrechten.l@northeastern.edu <br />
-            216-299-5637
+            or Personal: liuvone@outlook.com
         </p>
         <div className="social-icons">
         <a href="https://www.linkedin.com/in/liu-von-engelbrechten/" target="_blank">
