f1e684a7b8421e220171a9c7ab6c0b160ad92f1a
Liu von Engelbrechten <vonengelbrechten.l@northeastern.edu>
2025-12-02 22:02:19 -0500

    Added multiplayer section

--- src/projects/KitchenChaos.jsx
+++ src/projects/KitchenChaos.jsx
@@ -9,12 +9,12 @@ export default function KitchenChaos() {
                 <div className="projectOverview">
                     <h2>Project Overview</h2>
                     <p>
-                        In my free time, I’ve been learning and experimenting with Unity by following a 
+                        I’ve been learning and experimenting with Unity by following a 
                         tutorial called Kitchen Chaos. <br/> <br/>
-                        During this ongoing project, I've been exploring the 
+                        During this project, I've been exploring the 
                         fundamentals of game development including subscribers and listeners, event-driven programming,
-                        colliders, animation systems, camera behavior, and player input. <br/> <br/>
-                        <a href="https://play.unity.com/en/games/4f844095-bf08-4d68-8e09-d11a981a3ba0/kitchen-chaos" target="_blank" rel="noopener noreferrer">
+                        colliders, animation systems, camera behavior, relays, and host vs client authoritative. <br/> <br/>
+                        <a href="https://play.unity.com/en/games/37192677-74ce-4619-bc19-984ecf092ae3/kitchen-chaos-multiplayer" target="_blank" rel="noopener noreferrer">
                             Click Here To Play The Full Game
                         </a>
                     </p>
@@ -27,8 +27,14 @@ export default function KitchenChaos() {
 
             {/* Contribution */}
             <h2 className="heading">Development Milestones</h2>
-            <p>Each step below highlights concepts I implemented as part of building this game.</p>
+            <p>
+                This project was split into two phases: single and multiplayer.
+                <br/>
+                Each step below highlights concepts I implemented as part of building this game.
+            </p>
 
+            {/* Single Player */}
+            <h2>Singleplayer</h2>
             <section className="schematic-section">
                 <div className="schematic-grid">
                     {[
@@ -66,6 +72,53 @@ export default function KitchenChaos() {
                         src: "/img/imgKitchenChaos/MainMenuAndRebinding.mp4",
                         title: "Main Menu and Key Rebinding",
                         desc: "State machine and player input systems."
+                    },
+                    ].map((video, idx) => (
+                    <div className="schematic-card" key={idx}>
+                        <h3>{video.title}</h3>
+                        <p>{video.desc}</p>
+                        <video width="100%" controls>
+                        <source src={video.src} type="video/mp4" />
+                        Cannot load video.
+                        </video>
+                    </div>
+                    ))}
+                </div>
+            </section>
+
+            <br/>
+
+            {/* Multiplayer */}
+            <h2>Multiplayer</h2>
+            <p>
+                The second part of this project was allowing this game to be played on its own server
+                so that anyone can join a person's game. The first step was synchronizing all of the game states
+                so that normal game objects would be connected via network variables. This involved deciding whether
+                an action should be server or client authoritative.
+                <br/> <br/>
+                Using Unity's Netcode library, the game starts the player as either a host or client depending on
+                whether they create or join a lobby. Finally, I implemented Unity's Relay, to connect players
+                together even behind a firewall. This relay acted as an accessible/reachable server that any player could connect to.
+                All players connect to the relay and after connection is established, the Relay sends packets back and forth between the
+                connected Host and Clients. 
+            </p>
+            <section className="schematic-section">
+                <div className="schematic-grid">
+                    {[
+                    {
+                        src: "/img/imgKitchenChaos/SyncingGameState.mp4",
+                        title: "Syncing Game State",
+                        desc: "Network Behavior/Variable, Client vs Server Authoritative."
+                    },
+                    {
+                        src: "/img/imgKitchenChaos/MultiplayerCharacterSelection.mp4",
+                        title: "Joining Public Lobby",
+                        desc: "Authentication Service, Player Data"
+                    },
+                    {
+                        src: "/img/imgKitchenChaos/PrivateLobby.mp4",
+                        title: "Joining Private Lobbies",
+                        desc: "Authentication Service, Relay, Lobby UI"
                     },
                     ].map((video, idx) => (
                     <div className="schematic-card" key={idx}>
